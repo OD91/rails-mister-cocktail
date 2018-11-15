0868003ca40f80332838270c16fa15c725991091
ole dening <ole.dening@gmail.com>
2018-11-15 12:40:38 +0100

    models raked all green

---
+++ app/models/dose.rb
@@ -0,0 +1,12 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+end
+
+
+#A dose must have a description, a cocktail and an ingredient, and [cocktail, ingredient] pairings should be unique.
