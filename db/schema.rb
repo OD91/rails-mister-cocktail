7f12f4a236073658f350fa977e000d023488d9b7
ole dening <ole.dening@gmail.com>
2018-11-15 17:27:58 +0100

    pics now can be added through a link

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_111829) do
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
+    t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
